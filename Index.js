a6cfb923608225d9fcf8672c8e1b0f66328670f1
JuniorSarh <sabelohgumede@gmail.com>
2025-06-24 10:46:10 +0200

    refresher activity

---
+++ Index.js
@@ -0,0 +1,10 @@
+const Firstname = "Sabelo"
+const lastname = "Gumede"
+let age = undefined
+
+let isMale=true
+console.log(Firstname)
+if(Firstname.trim()=="")
+{
+    console.log("first name is required ")
+}

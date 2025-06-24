8ac61ca2d39f96859ab3184016cb768b3e473603
JuniorSarh <sabelohgumede@gmail.com>
2025-06-24 12:11:16 +0200

    updates

--- Index.js
+++ Index.js
@@ -6,5 +6,27 @@ let isMale=true
 console.log(Firstname)
 if(Firstname.trim()=="")
 {
-    console.log("first name is required ")
+    console.log("first name is required ");
 }
+else
+{
+    console.log(Firstname);
+}
+
+//Arrays
+
+let course =["dat structures","statistics","mathermatics"]
+course.push("database design")
+console.log(course);
+
+//nested arrays
+
+let modules =[
+    "maths101",
+    {newmodule:"info systems",
+    newmodule1:"databases"
+    },
+    "excel"
+]
+
+console.log(modules);
